f818b1a6140be8e4795d173722b73e23ea1261e0
lvhaosir6 <985694388@qq.com>
2021-05-10 00:54:19 +0800

    Initial commit

---
+++ src/cn/lvhaosir/test/TestMain.java
@@ -0,0 +1,17 @@
+package cn.lvhaosir.test;
+
+import cn.lvhaosir.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1","1");
+        map.put("1","1");
+        map.put("1","1");
+    }
+
+}
